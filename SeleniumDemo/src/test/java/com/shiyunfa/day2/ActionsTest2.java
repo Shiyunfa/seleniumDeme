e98a001b53654293ad30167ca7b5ab95d14d72e2
运发 <1290251646@qq.com>
2020-08-12 22:09:45 +0800

    添加模拟多选下拉框方法，模拟键盘方法，上传文件

--- SeleniumDemo/src/test/java/com/shiyunfa/day2/ActionsTest2.java
+++ SeleniumDemo/src/test/java/com/shiyunfa/day2/ActionsTest2.java
@@ -2,6 +2,7 @@ package com.shiyunfa.day2;
 
 import com.shiyunfa.day1.Tool;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
@@ -11,6 +12,10 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -27,10 +32,10 @@ public class ActionsTest2 {
 
 
     @Test
-    public  void rightClickTest() throws InterruptedException {
+    public  void rightClickTest() throws InterruptedException, AWTException {
         element = driver.findElement(By.id("su"));
 
-        Actions actions = new Actions(driver);
+       /* Actions actions = new Actions(driver);
         //在百度页面右键点击（传入要右键的元素）
         //actions.contextClick(element).perform();
         WebElement text = driver.findElement(By.id("kw"));
@@ -45,6 +50,24 @@ public class ActionsTest2 {
         actions.dragAndDropBy(element,500,500).perform();
         //拖动元素至某个元素     选中元素        移动元素    取消选中元素      执行
         actions.clickAndHold(text).moveToElement(text).release(text).perform();
+
+        //模拟一个下拉框
+        List<WebElement> elements = new ArrayList<WebElement>(3);
+        //多选下拉框     按住shift     点击第1个选项         点击第3个选项     松开shift             执行
+        actions.keyDown(Keys.SHIFT).click(elements.get(0)).click(elements.get(2)).keyUp(Keys.SHIFT).perform();*/
+
+
+        //int keys = (int)new Character('s') 转换为ascll
+        //robot模拟键盘操作   保存一个html
+        Robot robot = new Robot();
+        robot.keyPress(KeyEvent.VK_CONTROL);
+        robot.keyPress(KeyEvent.VK_S);
+        Thread.sleep(5000);
+        robot.keyPress(KeyEvent.VK_ENTER);
+        Thread.sleep(5000);
+
+        //上传文件      选中上传按钮          获取文件地址
+        driver.findElement(By.id("id")).sendKeys("");
 
     }
 
